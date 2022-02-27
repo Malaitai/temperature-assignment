ec13854870146406fc70053ac44ad2097270af21
Mr Malaitai (School) <49211523+Malaitai@users.noreply.github.com>
2022-02-28 10:36:07 +1300

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,27 @@
+# Student Name:
+# Date: 
+
+# 1. Write a print statement that says "Welcome to the Temperature Conversion program"
+print("")
+
+# 2. Ask the user if they want to convert from Farenheit to Celsius or vice versa (provide 2 options)
+choice = input("")
+
+# 3. Ask the user for a number to convert
+temp = int(input(""))
+
+# 4. Convert the number to its respective temperature
+farenheit = ["farenheit", "f"]
+celsius = ["celsius", "c"]
+result = 0
+
+if choice == farenheit:
+    pass
+elif choice == celsius:
+    pass
+else:
+    print("Please choose \'farenheit\' or \'celsius\'")
+
+
+# 5. Print the results
+print(result)
